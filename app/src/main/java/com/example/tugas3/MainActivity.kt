d020a28c26006f0621ed3141ef2cb50a65c646b2
ajisetyawan <ajisetyawan58@gmail.com>
2019-04-24 13:38:03 +0700

    pertama

---
+++ app/src/main/java/com/example/tugas3/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.tugas3
+
+import android.content.Intent
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_move_activity.setOnClickListener{
+            val intent = Intent(this,MoveActivity::class.java)
+            startActivity(intent)
+        }
+        btn_move_activity_data.setOnClickListener{
+            startActivity(Intent(this, MoveWithDataActivity::class.java))
+        }
+        btn_dial_number.setOnClickListener {
+            val intent = Intent(this,constraint::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
